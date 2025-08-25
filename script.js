c3ed7c210d5ecc9aab2283897d66430a77fa6787
Ashub Shafqat <ashubshafqat@gmail.com>
2025-08-25 16:33:42 +0500

    Initial commit.

--- script.js
+++ script.js
@@ -101,7 +101,7 @@ const portfolioData = {
             id: 6,
             title: "German Language Learning Buddy",
             description: "A German language learning buddy helps you practice speaking, learn new words, and improve grammar in a fun and easy way.",
-            image: "German language learning buddy.PNG",
+            image: "german.PNG",
             category: "generativeai",
             tech: ["Python", "Gemini", "Groq", "Flask","HTML/CSS"],
             github: "https://github.com/AshubShafqat/German-Language-Learning-Application",
